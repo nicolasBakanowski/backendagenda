cbc586c0c3f0e864876e97d8ab3c687e7dcc7cfa
nicolasBakanowski <63619842+nicolasBakanowski@users.noreply.github.com>
2021-07-23 18:04:22 -0300

    Update mongo.js
    change security to string conection

--- mongo.js
+++ mongo.js
@@ -1,4 +1,4 @@
-const connectionStr = 'mongodb+srv://nbaka:nbaka145236@cluster0.aw8wi.mongodb.net/agenda?retryWrites=true&w=majority'
+const connectionStr = 'mongodb+srv://<user>:<password>@cluster0.aw8wi.mongodb.net/<database>?retryWrites=true&w=majority'
 
 const mongoose = require('mongoose');
 module.exports = mongoose.connect(connectionStr, {useNewUrlParser: true, useUnifiedTopology: true})
